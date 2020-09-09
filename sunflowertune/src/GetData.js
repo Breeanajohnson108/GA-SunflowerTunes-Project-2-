b64691ea288dd053ae7e4b19e5db5a3b97293c0b
Breeana Johnson <breeanajohnson108@gmail.com>
2020-09-08 22:42:34 -0400

    Able to loop through api data to get each info for each tune

--- sunflowertune/src/GetData.js
+++ sunflowertune/src/GetData.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import axios from 'axios'
+import PlayList from './PlayList';
 
 export default class GetData extends React.Component
 {
@@ -15,17 +16,26 @@ export default class GetData extends React.Component
     fetch('https://api.airtable.com/v0/appwmk7KWbWQtvEEW/playlist?api_key=keyHS2cdOW12Q7yll').then(res => res.json()).then(
     res =>
       {
-        console.log(res.records);
-        this.setState({tunesData: res.records})
+        this.setState({tunesData: res.records});
+        // for (let property in data)
+        // {
+        //   let properties = data[property].fields;
+        //   let title = properties.title;
+        //   let artist = properties.artist;
+        //   let link = properties.link;
+        //   let coverImage = properties.thumbnail;
+        // }
       }
-    )
-    
+    ).catch(error => console.log(error))
   }
   
   render()
   {
+    let {tunesData} = this.state
     return (
-      null
+      tunesData.map(data => (
+        <PlayList {...data.fields} key={data.fields.id} />
+      ))
     )
   }
 }
