d4aa7b75186d9138e46b227ed6b1b9c2fe9b7a60
Breeana Johnson <breeanajohnson108@gmail.com>
2020-09-08 21:06:35 -0400

    Build first component that fetchs api data from airtable

--- sunflowertune/src/GetData.js
+++ sunflowertune/src/GetData.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import axios from 'axios'
 
-export default function GetData()
+export default class GetData extends React.Component
 {
-  return (
-    <div>
-      Hello      
-    </div>
-  )
+  
+  state = 
+    {
+      tunesData: []
+    }
+  
+  componentDidMount()
+  {
+    
+    fetch('https://api.airtable.com/v0/appwmk7KWbWQtvEEW/playlist?api_key=keyHS2cdOW12Q7yll').then(res => res.json()).then(
+    res =>
+      {
+        console.log(res.records);
+        this.setState({tunesData: res.records})
+      }
+    )
+    
+  }
+  
+  render()
+  {
+    return (
+      null
+    )
+  }
 }
