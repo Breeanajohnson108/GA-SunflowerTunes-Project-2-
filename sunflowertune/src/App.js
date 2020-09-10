7ebefbc69e5af900d2a3d47fc87b1fc096e97731
Breeana Johnson <breeanajohnson108@gmail.com>
2020-09-10 11:18:59 -0400

    added a create form

--- sunflowertune/src/App.js
+++ sunflowertune/src/App.js
@@ -2,8 +2,7 @@ import React from 'react';
 import PlayList from './PlayList'
 import GetData from './GetData'
 import PlayTunes from './Playtunes'
-import LikeTune from './LikeTune'
-import DislikeTune from './DislikeTune'
+
 
 function App()
 {
@@ -13,8 +12,6 @@ function App()
       <GetData />
       <PlayList />
       <PlayTunes />
-      <LikeTune />
-      <DislikeTune />
     </>
   )
 }
--- sunflowertune/src/Playtunes.js
+++ sunflowertune/src/Playtunes.js
@@ -1,38 +1,43 @@
 import React, { useState, state } from 'react'
 import GetData from './GetData'
 import './App.css'
-import PlayList from './PlayList' 
-import LikeTune from './LikeTune'
+import PlayList from './PlayList'
 
 
-export default function Playtunes({ artist, title, link, thumbnail })
-{  
+
+export default function Playtunes({ artist, title, link, thumbnail }) {
   let [showVid, setshowVid] = useState(false)
-  let [likes, setlikes] = useState(0,function (likes)
-  {
-    let newCount = likes + 1
-     console.log()
-   })
-  
+  let setlikes = 0;
   return (
-    
+
     <div className="vidContainer">
-          <button className="playMe" onClick=
-          {
-            () => setshowVid(!showVid)
-          }>Play Me</button>
+      <button className="playMe" onClick=
+        {
+          () => setshowVid(!showVid)
+        }>Play Me</button>
       {
         showVid &&
         <div>
           <iframe className="videos" width="560" height="315" src={link}
-          frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen ></iframe >
-          <button className="like" onClick={() => setlikes(likes)}>Likes: {likes}</button>
+          <button className="like" onClick={(setlikes) => setlikes++}>Likes: {setlikes}</button>
           <button className="disLike">Dislike</button>
-        </div>         
+          
+          {/* Creating a form */}
+          <form className="form">
+            <h1>Tunes Requests</h1>
+            <p>Enter embedded youtube link</p>
+            <input type="text" placeholder="Youtube Link....">
+            </input>
+            <button type="submit">
+              submit
+            </button>
+          </form>
+        </div>
       }
     </div>
-    
-    
+
+
   )
 }
